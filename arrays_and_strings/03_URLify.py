8053394a04da758811f5705d957db52f1f59f7e9
hermancai <30858461+hermancai@users.noreply.github.com>
2022-04-07 23:35:33 -0700

    Commit

---
+++ arrays_and_strings/03_URLify.py
@@ -0,0 +1,44 @@
+'''
+Replace all spaces in a string with '%20'.
+There is space at the end of the string to hold the extra chars.
+You are also given the true length of the string.
+
+Input:  "Mr John Smith    ", 13
+Output: "Mr%20John%20Smith"
+
+Solutions: 
+
+##### 1
+Turn string into list of chars for space efficiency.
+Loop through string backwards, starting at true length index.
+Check and replace chars starting at last index.
+Return joined list of chars.
+
+Time: O(n)
+Space: O(1)
+
+'''
+
+def replaceSpaces(s: str, i: int):
+  s = [x for x in s]  # Split string into char list
+
+  oldI = i - 1  # True index
+  newI = len(s) - 1
+
+  # Loop backwards through string, replacing chars
+  while oldI >= 0 and newI >= 0:
+    if s[oldI] == " ":
+      s[newI] = "0"
+      s[newI - 1] = "2"
+      s[newI - 2] = "%"
+      newI -= 3
+    else:
+      s[newI] = s[oldI]
+      newI -= 1
+    oldI -= 1
+
+  return "".join(s)
+
+if __name__ == "__main__":
+  s = "Mr John Smith    "
+  print(replaceSpaces(s, 13))
---
+++ arrays_and_strings/04_palindrome_permutation.py
@@ -0,0 +1,48 @@
+'''
+Decide if a string is the permutation of a palindrome.
+Assume only alphabet and spaces, not case sensitive.
+
+Input: "Tact Coa"
+Output: True ("taco cat", "atco cta", etc)
+
+Solutions:
+Property of a palindrome: it can have at most one odd count of a character
+
+##### 1:
+Given: string only contains alphabet chars and spaces
+
+Create list of size 26 to track char frequency.
+Loop through list.
+  Return false if more than one odd count.
+Return True.
+
+Time: O(n)
+Space: O(1)
+
+'''
+
+def palindromePermutation(s: str):
+  s = s.lower()
+  frequencyList = [0] * 26
+  offset = ord("a")
+
+  # Build frequency list
+  for c in s:
+    if not c.isalpha():
+      continue
+    frequencyList[ord(c) - offset] += 1
+
+  # Check for more than one odd count in frequency list
+  foundOdd = False
+  for val in frequencyList:
+    if val % 2 != 0:
+      if foundOdd:
+        return False
+      foundOdd = True
+
+  return True
+
+
+if __name__ == "__main__":
+  s = "Tact Coa"
+  print(palindromePermutation(s))
