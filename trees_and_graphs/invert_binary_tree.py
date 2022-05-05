b739da64c018498f7d3eee1dc6e7d3a4b7d47b14
Herman Cai <hermancai3@gmail.com>
2022-05-04 23:58:53 -0700

    Commit

--- trees_and_graphs/invert_binary_tree.py
+++ trees_and_graphs/invert_binary_tree.py
@@ -4,7 +4,14 @@ Invert a binary tree.
 Solutions:
 
 ##### 1
-Depth first in order traversal. Swap the child nodes.
+Depth first pre-order traversal. Swap the child nodes.
+
+Time: O(n)
+Space: O(d) -> O(n) where d is max depth of tree
+
+
+##### 2
+Iterative depth first traversal using stack. 
 
 Time: O(n)
 Space: O(d) where d is max depth of tree
@@ -14,12 +21,26 @@ Space: O(d) where d is max depth of tree
 from binary_node import BinaryNode as Node
 import print_binary_tree as p
 
+# Solution 1
 def invert(root: Node) -> None:
   if not root: return
-  
-  invert(root.left)
+
+  print(root.val)
   root.left, root.right = root.right, root.left
+  invert(root.left)
   invert(root.right)
+
+# Solution 2
+# NOTE: This is not breadth first traversal, which would use a queue.
+def invert2(root: Node) -> None:
+  stack = [root]
+
+  while stack:
+    node = stack.pop()
+    if node: 
+      print(node.val)
+      node.left, node.right = node.right, node.left
+      stack += node.left, node.right
 
 
 if __name__ == "__main__":
